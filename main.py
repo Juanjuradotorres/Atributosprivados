f148ae6747bf9b5ff34b072c8d935f2d504e2c23
Juanjuradotorres <juanjuradotorres09@gmail.com>
2024-10-15 14:43:17 -0600

    Holaaaa

---
+++ main.py
@@ -0,0 +1,10 @@
+from classVenta import Venta
+
+venta = Venta()
+venta.set_id_venta(1)
+venta.set_fecha("10/01/2021")
+venta.set_cliente("Juan Jurado")
+venta.set_productos({"Producto 1": 12, "Producto 2": 60, "Producto 3": 45})
+
+# Mostrar los detalles de la venta
+venta.mostrar_detalle()
